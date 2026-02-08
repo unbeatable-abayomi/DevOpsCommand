3b1ff55335579cf1c234b26513a0c418399acb58
unbeatable-abayomi <igwubor@outlook.com>
2026-02-08 18:26:08 +0100

    Added Resource Block S3 bucket

--- abi-devops-demo/backend/main.tf
+++ abi-devops-demo/backend/main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = "us-west-2"
 }
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
