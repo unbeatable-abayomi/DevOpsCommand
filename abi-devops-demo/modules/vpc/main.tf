fafe835edb42971c27f1242346fec5deecbf04fc
unbeatable-abayomi <igwubor@outlook.com>
2026-02-08 22:06:15 +0100

    aws_route_table_association public

--- abi-devops-demo/modules/vpc/main.tf
+++ abi-devops-demo/modules/vpc/main.tf
@@ -42,3 +42,9 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
